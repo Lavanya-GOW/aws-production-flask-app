7a3b25c7cd3157818c01888bf957f95b16706a90
Lavanya Sharma <sharmalavanya1710@gmail.com>
2026-06-13 07:19:58 +0530

    Add first Github actions workflow and Docker

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
+import os
+import redis
 
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    decode_responses=True
+)
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AWS Production Flask App Running!"
+    count = redis_client.incr("visitor_count")
+    return f"""
+    <h1>AWS Production Flask App!</h1>
+    <p>Visitor count: {count}</p>
+    """
 
 @app.route("/health")
 def health():
-    return "Application Healthy"
+    try:
+        redis_client.ping()
+        return "Application Healthy"
+    except Exception as e:
+        return f"Application Unhealthy: {str(e)}"
 
 @app.route("/about")
 def about():
